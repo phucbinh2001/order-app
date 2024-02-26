3e811e475dcee5aef407e7343c59828c286ad15d
Phuc Binh <phucbinh.2001@gmail.com>
2024-02-26 18:32:59 +0700

    implement dnd

--- components/OrderList/OrderList.tsx
+++ components/OrderList/OrderList.tsx
@@ -1,22 +1,45 @@
-import { Order } from "@/types/order";
-import React, { useState } from "react";
-import OrderCard from "../OrderCard/OrderCard";
 import useOrderStore from "@/store/orderStore";
+import { Order } from "@/types/order";
+import GridLayout from "react-grid-layout";
+import OrderCard from "../OrderCard/OrderCard";
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
 
 const OrderList = ({ orders }: { orders: Order[] }) => {
   const setSelectedOrder = useOrderStore((state) => state.setSelectedOrder);
   const selectedOrder = useOrderStore((state) => state.selectedOrder);
+  const layout = [
+    { i: "65daafc3d6090135a72881f7", x: 0, y: 0, w: 1, h: 4, autoSize: true },
+    { i: "65daad70dce3f89f58574753", x: 1, y: 0, w: 1, h: 4, autoSize: true },
+  ];
   return (
-    <div className="order-list grid grid-cols-4 bg-[#4e4cb8] rounded-2xl p-4 gap-4">
-      {orders.map((item, index) => (
-        <div onClick={() => setSelectedOrder(item)}>
-          <OrderCard
-            active={item._id == selectedOrder?._id}
-            order={item}
-            key={index}
-          />
-        </div>
-      ))}
+    <div className="order-list bg-[#4e4cb8] rounded-2xl p-4">
+      {orders.length > 0 && (
+        <GridLayout
+          autoSize={true}
+          style={{
+            width: "100%",
+            minHeight: "100vh",
+            backgroundSize: "100% 100%",
+            overflow: "hidden",
+          }}
+          cols={4}
+          rowHeight={50}
+          width={1200}
+          layout={layout}
+          isResizable={false}
+        >
+          {orders.map((item, index) => (
+            <div key={item._id} onClick={() => setSelectedOrder(item)}>
+              <OrderCard
+                active={item._id == selectedOrder?._id}
+                order={item}
+                key={index}
+              />
+            </div>
+          ))}
+        </GridLayout>
+      )}
     </div>
   );
 };
