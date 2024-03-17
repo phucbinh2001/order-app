eab70764dfe0c4fc2e20242805812bc385e87652
shibaInu89 <162393047+shibaInu89@users.noreply.github.com>
2024-03-17 22:25:35 +0700

    add analytic

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.scss";
 import { ConfigProvider, ThemeConfig } from "antd";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -50,6 +51,7 @@ export default function RootLayout({
         <ConfigProvider theme={theme}>
           <AntdRegistry>{children}</AntdRegistry>
         </ConfigProvider>
+        <Analytics />
       </body>
     </html>
   );
