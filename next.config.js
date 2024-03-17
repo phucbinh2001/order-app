a05c79fa4bfbdffd259f9174b29cbc228a6296e7
Phuc Binh <phucbinh.2001@gmail.com>
2024-03-17 13:11:15 +0700

    fix deploy error

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true, // Enable React strict mode for improved error handling
   swcMinify: true, // Enable SWC minification for improved performance
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development", // Remove console.log in production
   },
